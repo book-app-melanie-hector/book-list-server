364673d7b8151ce14aa0b5f7a321f8ca4cc2f982
Hector <norzagaray@me.com>
2017-12-12 10:02:20 -0800

    commented out code on master branch ment to work on separate branch

--- server.js
+++ server.js
@@ -22,38 +22,36 @@ app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 //CLIENT_URL=http://localhost:8080
 //DATABASE_URL=postgres://localhost:5432/books_app
 
-function loadDB() {
-  client.query(`
-  CREATE TABLE books (
-    book_id serial primary key,
-    author varchar(255) not null,
-    title varchar(255) not null,
-    isbn varchar(30),
-    image_url varchar(255),
-    description TEXT NOT NULL);
-    `)
-}
-
-
-
-function loadBooks () {
-  client.query('SELECT COUNT (*) FROM books')
-    .then(result => {
-      if(!parseInt(result.rows[0].count)) {
-        fs.readFile('./book-list-client/data/books.json', 'utf8', (err, fd) => {
-          JSON.parse(fd).forEach(ele => {
-            client.query(`
-              INSERT INTO
-              books(title, author, isbn, image_url, description)
-              SELECT book_id, &1, $2, $3, $4, $5;
-              `,
-                [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
-            )
-            .catch(console.error);
-          })
-        }
-      )
-    }
-  })
-}
-loadBooks();
+// function loadDB() {
+//   client.query(`
+//   CREATE TABLE books (
+//     book_id serial primary key,
+//     author varchar(255) not null,
+//     title varchar(255) not null,
+//     isbn varchar(30),
+//     image_url varchar(255),
+//     description TEXT NOT NULL);
+//     `)
+// }
+//
+// function loadBooks () {
+//   client.query('SELECT COUNT (*) FROM books')
+//     .then(result => {
+//       if(!parseInt(result.rows[0].count)) {
+//         fs.readFile('./book-list-client/data/books.json', 'utf8', (err, fd) => {
+//           JSON.parse(fd).forEach(ele => {
+//             client.query(`
+//               INSERT INTO
+//               books(title, author, isbn, image_url, description)
+//               SELECT book_id, &1, $2, $3, $4, $5;
+//               `,
+//                 [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
+//             )
+//             .catch(console.error);
+//           })
+//         }
+//       )
+//     }
+//   })
+// }
+// loadBooks();
