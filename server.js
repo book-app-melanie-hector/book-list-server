e0f86d76dc70ffc8c8ee37e78cbcaabf8ec7ae97
Melanie Downing <sayanything830@gmail.com>
2017-12-12 18:23:57 -0800

    it's working

--- server.js
+++ server.js
@@ -23,28 +23,13 @@ app.get('/api/v1/books', (request, response) => {
     .catch(console.error);
 })
 
-app.get('*', (req,res) => res.redirect(CLIENT_URL));
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 //PORT=3000
 //CLIENT_URL=http://localhost:8080
 //DATABASE_URL=postgres://localhost:5432/books_app
 
-function loadDB() {
-  client.query(`
 
-  CREATE TABLE IF NOT EXISTS books(
-    book_id SERIAL PRIMARY KEY,
-    author VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    isbn VARCHAR(30),
-    image_url VARCHAR(255),
-    description TEXT NOT NULL);
-    `)
-    .then(loadBooks())
-    .catch(console.error);
-}
 
 //
 //
@@ -69,17 +54,38 @@ function loadBooks () {
   client.query('SELECT COUNT(*) FROM books')
     .then(result => {
       if(!parseInt(result.rows[0].count))
-        fs.readFile('./book-list-client/data/books.json', 'utf-8', (err, fd) => {
+        fs.readFile('../book-list-client/data/books.json', 'utf8', (err, fd) => {
           JSON.parse(fd).forEach(ele => {
             client.query(`
               INSERT INTO
               books (title, author, isbn, image_url, description)
               VALUES ($1, $2, $3, $4, $5)
-              ON CONFLICT (isbn) DO NOTHING;`,
+              `,
                 [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
             )
-            // .catch(console.error);
+            .catch(console.error);
           })
         })
       })
   }
+
+  function loadDB() {
+    console.log('loadDB triggered');
+    client.query(`
+
+    CREATE TABLE IF NOT EXISTS books(
+      book_id SERIAL PRIMARY KEY,
+      author VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      isbn VARCHAR(30),
+      image_url VARCHAR(255),
+      description TEXT NOT NULL);
+      `)
+      .then(loadBooks)
+      .catch(console.error);
+  }
+  loadDB();
+
+  app.get('*', (req,res) => res.redirect(CLIENT_URL));
+
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
